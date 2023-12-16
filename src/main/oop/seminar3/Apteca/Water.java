e62749ab4771c33cfd4221e6046491c577979453
monus39 <114568451+monus39@users.noreply.github.com>
2023-12-17 02:11:24 +0300

    Delete src/main/oop/seminar3/Apteca directory

--- src/main/oop/seminar3/Apteca/Water.java
+++
@@ -1,7 +0,0 @@
-package src.main.oop.seminar3.Apteca;
-
-public class Water extends PharmancyComponent{
-    public Water(String name, String wiegth, int power) {
-        super(name, wiegth, power);
-    }
-}
